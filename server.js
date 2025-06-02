89ce3e0fa07fb16d8ad2085d39281d42bf518e0f
4L302 <alegarg2002@gmail.com>
2025-06-02 19:29:00 +0200

    .

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use('/upload', uploadRoute);
+app.use('/api/upload', uploadRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
